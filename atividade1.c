d7f17b007a322749ce42e6e2074fde55977b5e97
kevile-wee <kevilevictoria871@gmail.com>
2024-10-08 08:12:26 -0300

    Add files via upload

---
+++ atividade1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main (){
+
+    float peso;
+    float altura;
+    float c;
+    float a;
+    float b;
+
+    printf("digite seu peso:");
+    scanf("%f" , &peso);
+    printf("digite sua altura:");
+    scanf("%f" , &altura);
+    //printf(%f %f , &a, &b);
+    c = a /(b * b);
+    printf("o valor de Imc é: %.1f\n");
+    return 0;
+
+}
